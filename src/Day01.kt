12b7d6a7b758dc99e22045df1b5d3052fefa264d
Seongha Hong <dkqk0124@naver.com>
2023-12-02 00:22:52 +0900

    Day01 Refactor

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,73 +1,77 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.sumOf {
-            val first = it.find { it.isDigit() } ?: 0
-            val last = it.findLast { it.isDigit() } ?: 0
-            "$first$last".toInt()
+        return input.sumOf { str ->
+            val lastIndex = str.lastIndex
+            var first: Char? = null
+            var last: Char? = null
+            var sum = 0
+
+            str.indices.forEach { idx ->
+                if (str[idx].isDigit() && first == null) first = str[idx]
+                if (str[lastIndex - idx].isDigit() && last == null) last = str[lastIndex - idx]
+
+                if (first != null && last != null) {
+                    sum = "$first$last".toInt()
+                    return@forEach
+                }
+            }
+
+            sum
         }
     }
 
     fun part2(input: List<String>): Int {
-
         var sum = 0
 
         input.forEach { str ->
+            val lastIndex = str.lastIndex
+
             var firstTemp = ""
             var lastTemp = ""
 
-            for (c in str) {
-                firstTemp += c
+            var isFindFirst = false
+            var isFindLast = false
 
-                val num = when {
-                    c.isDigit() -> c.digitToInt()
-                    firstTemp.contains("one") -> 1
-                    firstTemp.contains("two") -> 2
-                    firstTemp.contains("three") -> 3
-                    firstTemp.contains("four") -> 4
-                    firstTemp.contains("five") -> 5
-                    firstTemp.contains("six") -> 6
-                    firstTemp.contains("seven") -> 7
-                    firstTemp.contains("eight") -> 8
-                    firstTemp.contains("nine") -> 9
-                    else -> 0
+            str.indices.forEach { idx ->
+                firstTemp += str[idx]
+                lastTemp = str[lastIndex - idx] + lastTemp
+
+                val firstNum = if (str[idx].isDigit()) str[idx].digitToInt()
+                else firstTemp.convertStringToNumber()
+                val lastNum = if (str[lastIndex - idx].isDigit()) str[lastIndex - idx].digitToInt()
+                else lastTemp.convertStringToNumber()
+
+                if (firstNum != 0 && isFindFirst.not()) {
+                    sum += firstNum.times(10)
+                    isFindFirst = true
                 }
-
-                if (num != 0) {
-                    sum += num.times(10)
-                    firstTemp = ""
-                    break
-                }
-            }
-
-            for (c in str.reversed()) {
-                lastTemp = c + lastTemp
-
-                val num = when {
-                    c.isDigit() -> c.digitToInt()
-                    lastTemp.contains("one") -> 1
-                    lastTemp.contains("two") -> 2
-                    lastTemp.contains("three") -> 3
-                    lastTemp.contains("four") -> 4
-                    lastTemp.contains("five") -> 5
-                    lastTemp.contains("six") -> 6
-                    lastTemp.contains("seven") -> 7
-                    lastTemp.contains("eight") -> 8
-                    lastTemp.contains("nine") -> 9
-                    else -> 0
-                }
-
-                if (num != 0) {
-                    sum += num
-                    lastTemp = ""
-                    break
+                if (lastNum != 0 && isFindLast.not()) {
+                    sum += lastNum
+                    isFindLast = true
                 }
             }
         }
-
         return sum
     }
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+/**
+ * 주어진 String에 one, two와 같은 숫자 단어가 들어가 있는 경우, 해당 숫자 단어를 반환하는 확장 함수.
+ * Day01 문제에 'zero' 는 해당 사항이 없었으므로 조건문에서 제외.
+ */
+fun String.convertStringToNumber(): Int = when {
+    contains("one") -> 1
+    contains("two") -> 2
+    contains("three") -> 3
+    contains("four") -> 4
+    contains("five") -> 5
+    contains("six") -> 6
+    contains("seven") -> 7
+    contains("eight") -> 8
+    contains("nine") -> 9
+    else -> 0
 }

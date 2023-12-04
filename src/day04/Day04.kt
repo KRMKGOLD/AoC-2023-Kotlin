761f5e5949446d3e77a3d8179fe6f9e1399695ec
Seongha Hong <dkqk0124@naver.com>
2023-12-04 12:58:31 +0900

    Update Readme, Add Day04 format

---
+++ src/day04/Day04.kt
@@ -0,0 +1,22 @@
+package day04
+
+import println
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        var sum = 0
+
+        return sum
+    }
+
+    fun part2(input: List<String>): Int {
+        var sum = 0
+
+        return sum
+    }
+
+    val input = readInput("day04/Day04")
+    part1(input).println()
+    part2(input).println()
+}

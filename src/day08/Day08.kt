360cae7e195cd665542b9099c954a410b83ed2ea
Seongha Hong <dkqk0124@naver.com>
2023-12-08 14:49:24 +0900

    Day08 template

---
+++ src/day08/Day08.kt
@@ -0,0 +1,18 @@
+package day08
+
+import println
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val input = readInput("day08/Day08_test")
+    part1(input).println()
+    part2(input).println()
+}
